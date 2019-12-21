fa2a2fafb9cc39be3e483aa01487582d001f9d94
Alejandro Martin Perez <alexandermpf77@gmail.com>
2019-12-21 15:08:35 +0100

    Edit Note

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -10,44 +10,69 @@ import 'react-datepicker/dist/react-datepicker.css'
 export default class CreateNote extends Component {
 
     state = {
-        users: [],
-        userSelected: '',
         title: '',
         content: '',
-        date: new Date()
+        date: new Date(),
+        userSelected: '',
+        users: [],
+        editing: false,
+        _id: ''
     }
 
     async componentDidMount() {
-       const res = await axios.get('http://localhost:4000/api/users');
-       this.setState({
-           users: res.data.map(user => user.username),
-           userSelected: res.data[0].username
-       })
-       this.setState({users: res.data.map(user => user.username)});
-       
+        const res = await axios.get('http://localhost:4000/api/users');
+        if (res.data.length > 0) {
+            this.setState({
+                users: res.data.map(user => user.username),
+                userSelected: res.data[0].username
+            })
+        }
+        if (this.props.match.params.id) {
+            console.log(this.props.match.params.id)
+            const res = await axios.get('http://localhost:4000/api/notes/' + this.props.match.params.id);
+            console.log(res.data)
+            this.setState({
+                title: res.data.title,
+                content: res.data.content,
+                date: new Date(res.data.date),
+                userSelected: res.data.author,
+                _id: res.data._id,
+                editing: true
+            });
+        }
     }
 
     onSubmit = async (e) => {
-        //console.log(this.state.title, this.state.content)
         e.preventDefault();
-        const newNote = {
-            title: this.state.title,
-            content: this.state.content,
-            date: this.state.date,
-            author: this.state.userSelected
-        };
-        await axios.post('http://localhost:4000/api/notes', newNote);
+        if (this.state.editing) {
+            const updatedNote = {
+                title: this.state.title,
+                content: this.state.content,
+                author: this.state.userSelected,
+                date: this.state.date
+            };
+            await axios.put('http://localhost:4000/api/notes/' + this.state._id, updatedNote);
+        } else {
+            const newNote = {
+                title: this.state.title,
+                content: this.state.content,
+                author: this.state.userSelected,
+                date: this.state.date
+            };
+            axios.post('http://localhost:4000/api/notes', newNote);
+        }
         window.location.href = '/';
+
     }
 
-    onInputChange = e => {
+    onInputChange = (e) => {
         this.setState({
-           [e.target.name]: e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
     onChangeDate = date => {
-        this.setState({date});
+        this.setState({ date });
     }
 
     render() {
@@ -56,54 +81,48 @@ export default class CreateNote extends Component {
                 <div className="col-md-6 offset-md-3">
                     <div className="card card-body">
                         <h4>Create a Note</h4>
-
                         {/* SELECT USER */}
-
-
-
                         <form onSubmit={this.onSubmit}>
                             <div className="form-group">
                                 <select
                                     className="form-control"
                                     name="userSelected"
                                     onChange={this.onInputChange}
+                                    value={this.state.userSelected}
                                 >
                                     {
-                                        this.state.users.map(user => 
+                                        this.state.users.map(user =>
                                             <option key={user} value={user}>
                                                 {user}
                                             </option>
-                                            )
+                                        )
                                     }
                                 </select>
                             </div>
                             <div className="form-group">
-                                <input 
+                                <input
                                     type="text"
                                     name="title"
                                     placeholder="Tile"
                                     className="form-control"
                                     onChange={this.onInputChange}
+                                    value={this.state.title}
                                     required
                                 />
                             </div>
                             <div className="form-group">
-                                <textarea 
-                                name="content"
-                                className="form-control"
-                                placeholder="Content"
-                                onChange={this.onInputChange}
-                                required
+                                <textarea
+                                    name="content"
+                                    className="form-control"
+                                    placeholder="Content"
+                                    onChange={this.onInputChange}
+                                    value={this.state.content}
+                                    required
                                 ></textarea>
                             </div>
                             <div className="form-group">
-                                <DatePicker 
-                                    className="form-control"
-                                    selected={this.state.date}
-                                    onChange={this.onChangeDate}
-                                    //locale="es"
-                                />
-                            </div>
+                            <DatePicker className="form-control" selected={this.state.date} onChange={this.onChangeDate} />
+                        </div>
                             <button type="submit" className="btn btn-primary">Save</button>
                         </form>
                     </div>
